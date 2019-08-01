bde4d105328dc2c011593f6c4a8b2cf8a2183790
Ikju Son <ikju.son@sk.com>
2019-08-01 09:33:28 +0900

    [6장. AOP] 리스트 6-42 포인트컷까지 적용한 ProxyFactoryBean

--- src/test/java/com/sonpen/proxy/HelloTargetTest.java
+++ src/test/java/com/sonpen/proxy/HelloTargetTest.java
@@ -5,6 +5,8 @@ import org.aopalliance.intercept.MethodInvocation;
 import org.junit.Test;
 import org.mockito.cglib.proxy.MethodProxy;
 import org.springframework.aop.framework.ProxyFactoryBean;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
 
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
@@ -63,6 +65,23 @@ public class HelloTargetTest {
                                                                     // MethodInvocation은 메소드 정보와 함께 타깃 오브젝트를 알고 있기 때문이다.
             return ret.toUpperCase();       // 부가기능 적용
         }
+    }
+
+    @Test
+    public void pointcutAdvisor() {
+        ProxyFactoryBean pfBean = new ProxyFactoryBean();
+        pfBean.setTarget(new HelloTarget());
+
+        NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();       // 메소드 이름을 비교해서 대상을 선정하는 알고리즘을 제공하는 포인트 컷 생성
+        pointcut.setMappedName("sayH*");        // 이름 비교조건 설정. sayH로 시작하는 모든 메소드를 선택하게 한다.
+
+        pfBean.addAdvisor(new DefaultPointcutAdvisor(pointcut, new UppercaseAdvice()));     // 포인트컷과 어드바이스를 Advisor로 묶어서 한번에 추가.
+
+        Hello proxiedHello = (Hello)pfBean.getObject();
+
+        assertThat(proxiedHello.sayHello("Toby"), is("HELLO TOBY"));
+        assertThat(proxiedHello.sayHi("Toby"), is("HI TOBY"));
+        assertThat(proxiedHello.sayThankYou("Toby"), is("Thank You Toby"));     // 메소드 이름이 포인트컷의 선정조건에 맞지 않으므로, 부가기능이적용되지 않는다.
     }
 
 }

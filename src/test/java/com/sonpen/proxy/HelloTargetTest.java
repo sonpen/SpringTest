38ec558dbc56ec961ff24d0756f39ebc04d001d2
Ikju Son <ikju.son@sk.com>
2019-08-01 09:16:10 +0900

    [6장. AOP] 리스트 6-41 스프링 ProxyFactoryBean 을 이용한 다이내믹 프록시 테스트

--- src/test/java/com/sonpen/proxy/HelloTargetTest.java
+++ src/test/java/com/sonpen/proxy/HelloTargetTest.java
@@ -1,7 +1,12 @@
 package com.sonpen.proxy;
 
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
 import org.junit.Test;
+import org.mockito.cglib.proxy.MethodProxy;
+import org.springframework.aop.framework.ProxyFactoryBean;
 
+import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
 import static org.hamcrest.core.Is.is;
@@ -36,6 +41,28 @@ public class HelloTargetTest {
         assertThat(proxiedHello.sayHello("Toby"), is("HELLO TOBY"));
         assertThat(proxiedHello.sayHi("Toby"), is("HI TOBY"));
         assertThat(proxiedHello.sayThankYou("Toby"), is("THANK YOU TOBY"));
+    }
+
+    @Test
+    public void proxyFactoryBean() {
+        ProxyFactoryBean pfBean = new ProxyFactoryBean();
+        pfBean.setTarget(new HelloTarget());        // 타깃 설정
+        pfBean.addAdvice(new UppercaseAdvice());    // 부가기능을 담은 어드바이스를 추가한다. 여러개를 추가할 수도 있다.
+
+        Hello proxiedHello = (Hello)pfBean.getObject();     // FactoryBean이므로 getObject()로 생성된 프록시를 가져온다.
+
+        assertThat(proxiedHello.sayHello("Toby"), is("HELLO TOBY"));
+        assertThat(proxiedHello.sayHi("Toby"), is("HI TOBY"));
+        assertThat(proxiedHello.sayThankYou("Toby"), is("THANK YOU TOBY"));
+    }
+
+    static class UppercaseAdvice implements MethodInterceptor {
+        @Override
+        public Object invoke(MethodInvocation methodInvocation) throws Throwable {
+            String ret = (String) methodInvocation.proceed();       // 리플렉션의 Method와 달리 메소드 실행 시 타깃 오브젝트를 전달할 필요가 없다.
+                                                                    // MethodInvocation은 메소드 정보와 함께 타깃 오브젝트를 알고 있기 때문이다.
+            return ret.toUpperCase();       // 부가기능 적용
+        }
     }
 
 }
